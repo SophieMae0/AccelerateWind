35bb0dbdc9b99e1cfa0ae8e552ffe5e3e4cd0e46
SophieMae0 <sophie.schaffer@students.olin.edu>
2019-08-02 01:48:31 -0500

    started cost curve

--- wind_data_anlysis.py
+++ wind_data_anlysis.py
@@ -66,7 +66,7 @@ def velocity_function(u_array,v_array,angle,width):
     #the lower the angle difference the more power the wind generates
     angle_difference_width = angle_difference-width/2
     angle_difference = np.where(angle_difference>(2*math.pi-width/2), 0, angle_difference)
-    angle_difference = np.where(angle_difference_width>0, 0, angle_difference)
+    angle_difference = np.where(angle_difference_width<0, 0, angle_difference)
     #velocity of the wind that can be collected by the turbine
     velocity_array = np.cos(angle_difference)*speed_array
     #if the velcity is negative it is turned to zero
@@ -130,9 +130,10 @@ def coordinate_info(u_array,v_array,num_angles,width):
     wind_info = velocity_function(u_array,v_array,angles[0],width)
     return angles,wind_info
 
-def generator_energy(u_array,v_array,num_angles,width,gen_size):
+def generator_energy(power_array,gen_size):
     """Inputs:
     u_array and v_array: wind vectors in the u and v directions in m/s
+    power_array: power of wind turbine facing in the optimal direction ()
     num_angles: number of different angles the wind turbine could be facing
                 (equally distributed around a 2 pi radian circle)
     width: width in radians that wind can be collected with no loss
@@ -141,8 +142,6 @@ def generator_energy(u_array,v_array,num_angles,width,gen_size):
     energy_percents: an array of the percentage energy produced at each
                      location for each different generator size
     """
-    info = coordinate_info(u_array,v_array,num_angles,width)
-    power_array = info[1][0] #power collected at optimal angle
     #max wind power that can be collected
     max = power_function(gen_size)
     #power_difference will be negative if power is above max
@@ -155,7 +154,7 @@ def generator_energy(u_array,v_array,num_angles,width,gen_size):
     return new_energy
 
 
-def all_generators_energy(u_array,v_array,num_angles,width,num_gen):
+def all_generators_energy(power_array,original_energy,num_gen):
     """Inputs:
     u_array and v_array: wind vectors in the u and v directions in m/s
     num_angles: number of different angles the wind turbine could be facing
@@ -167,19 +166,17 @@ def all_generators_energy(u_array,v_array,num_angles,width,num_gen):
     energy_percents: an array of the percentage energy produced at each
                      location for each different generator size
     """
-    info = coordinate_info(u_array,v_array,num_angles,width)
-    original_energy = info[0][1] #energy collected with no gen limit
     energy_percents = []
     for i in range(num_gen): #number of dif sized generators
         #iterating over generators between size 6 to 12 (m/s)
-        gen_energy = generator_energy(u_array,v_array,num_angles,width,6+(i/num_gen)*6)
+        gen_energy = generator_energy(power_array,6+(i/(num_gen-1))*6)
         #percentage difference between original energy and energy with this generator
         energy_difference = gen_energy/original_energy
         energy_percents.append(energy_difference)
     #percentage difference between original energy and energy with all diferent generators
     return np.array(energy_percents)
 
-def generator_classification(u_array,v_array,num_angles,width,num_gen,best_percent):
+def generator_classification(power_array,original_energy,num_gen,best_percent):
     """Inputs:
     u_array and v_array: wind vectors in the u and v directions in m/s
     num_angles: number of different angles the wind turbine could be facing
@@ -195,7 +192,7 @@ def generator_classification(u_array,v_array,num_angles,width,num_gen,best_perce
                        generators collect
     """
     #percentage of optimal energy with num_gen different genrators
-    percent_all_gen = all_generators_energy(u_array,v_array,num_angles,width,num_gen)
+    percent_all_gen = all_generators_energy(power_array,original_energy,num_gen)
     #all percentages higher than best_percent are zero
     percent_below = np.where(percent_all_gen>=best_percent,0,percent_all_gen)
     #determining which generator is closest and lower than best_percent
@@ -210,11 +207,11 @@ def generator_classification(u_array,v_array,num_angles,width,num_gen,best_perce
             #if second best percent is the highest generator
             if i == num_gen-1:
                 best_percent = percent_all_gen[i]
-                best_gen = 6+((i)/num_gen)*6
+                best_gen = 6+(i/(num_gen-1))*6
             #best gen is one greater than second best
             else:
                 best_percent = percent_all_gen[i+1]
-                best_gen = 6+((i+1)/num_gen)*6
+                best_gen = 6+(i/(num_gen-1))*6
             above_best = False
             break
         #if all percentages are above best percent
@@ -222,104 +219,216 @@ def generator_classification(u_array,v_array,num_angles,width,num_gen,best_perce
             #adding the lowest percentage
             best_percent = percent_all_gen[0]
             #adding the smallest generator
-            best_gen = 6+((0)/num_gen)*6
+            best_gen = 6
     return best_gen,best_percent
 
-def flywheel(u_array,v_array,num_angles,width,gen_size,fly_size):
-    info = coordinate_info(u_array,v_array,num_angles,width)
-    power_array = info[1][0] #power collected at optimal angle
+def flywheel_energy(power_array,gen_size,fly_size):
     #max wind power that can be collected in genertor and flywheel
     max_gen = power_function(gen_size)
-    max_fly = power_function(fly_size)
+    max_fly = fly_size
     #power_difference will be negative if power is above max
-    power_difference = power_array - max_gen
-    #if power_difference is negative power is set to max
-    fly_power = np.where(power_difference>0, power_difference, 0)
-    fly_power = np.where(fly_power>max_fly, max_fly, fly_power)
+    power_dif = power_array - max_gen
 
-    #convert to energy (J)
-    new_energy = simps(fly_power)*60 #60 is converting from minutes to secnds
-    #sum up energy over time
-    return new_energy
+    fly_power_list = [0]
+
+    for i in range(len(power_dif)):
+        fly_power = fly_power_list[i]
+        #putting enegry back into the generator
+        if power_dif[i] < 0: #if energy is going back into the generator
+            power_array[i] += fly_power
+            fly_power += power_dif[i]
+            if fly_power < 0:
+                fly_power = 0 #if flywheel energy is negative
+        #changing power in flyheel
+        else:
+            fly_power += power_dif[i]
+            if fly_power > max_fly: #if flywheel energy is higher than max
+                fly_power = max_fly
+
+        fly_power_list.append(fly_power)
+
+    np.where(power_array>max_gen,max_gen,power_array)
+
+    fly_energy = simps(fly_power_list)*60
+    gen_energy = simps(power_array)*60
+    return(fly_energy,gen_energy)
 
 
+def all_flywheel_gen_energy(power_array,num_gen,num_fly):
+    max_fly = power_function(12)
+    gen_energy_array = []
+    for i in range(num_gen):
+        print('i is' , i)
+        gen_energy_array.append([])
+        for j in range(num_fly):
+            print('j is' , j)
+            #num gen is distributed between 6 and 12
+            #num fly is distributed between
+            fly_energy, gen_energy = flywheel_energy(power_array,6+(i/(num_gen-1))*6,((j+1)/(num_fly)*max_fly))
+            gen_energy_array[i].append(gen_energy)
+    return(np.array(gen_energy_array))
+
+def cost_curve(energy,gen_size,flywheel_size):
+    gen_size = power_function(gen_size)
+    mech_cost = 1118
+    inverter_rate = .35
 
 
-
-
-
-
+def classification():
+    pass
 
 
 
 
                 ###CALCULATING VALUES FOR GRAPHS
+#
+# os.chdir('/media/sophie/3aad97f1-cb33-412d-b7f3-a82f0fc88a34/fiveMinutes10')
+# width = math.pi
+# x = []
+# y = []
+# gen_list = []
+# energy_list = []
+# angle_list = []
+# all_angle_list = []
+# power_list = []
+# speed_list = []
+# velocity_list = []
+# flywheel_list = []
+# flywheel_gen_list = []
+# flywheel_gen_plain_list = []
+# all_flywheel_list = []
+# i = 0
+# for file in glob.glob("*"): #for each file in any folder
+#     i += 1
+#     print(i)
+#     print(file)
+#     coord = eval(file)
+#     u10,v10,direction = (get_data(file))
+#     u10 = np.delete(u10,slice(103000,103500))
+#     v10 = np.delete(v10,slice(103000,103500))
+#     direction = np.delete(direction,slice(103000,103500))
+#     info = coordinate_info(u10,v10,16,math.pi)
+    # #
+    # flywheel,flywheel_gen = flywheel_energy(info[1][0],6,6)
+    # flywheel = flywheel/3 #divided by 3 to make it annual
+    # flywheel_gen = flywheel_gen/3 #divided by 3 to make it annual
+    # flywheel_gen_plain = generator_energy(info[1][0],6)/3
 
-os.chdir('/media/sophie/3aad97f1-cb33-412d-b7f3-a82f0fc88a34/fiveMinutes10')
-width = math.pi
-x = []
-y = []
-gen_list = []
-energy_list = []
-angle_list = []
-all_angle_list = []
-power_list = []
-speed_list = []
-velocity_list = []
-i = 0
-for file in glob.glob("*"): #for each file in any folder
-    i += 1
-    print(i)
-    print(file)
-    coord = eval(file)
-    u10,v10,direction = (get_data(file))
-    u10 = np.delete(u10,slice(103000,103500))
-    v10 = np.delete(v10,slice(103000,103500))
-    direction = np.delete(direction,slice(103000,103500))
-    gen = generator_classification(u10,v10,16,math.pi,10,.8)[0]
-    info = coordinate_info(u10,v10,16,math.pi)
-    energy = info[0][1]/3 #divided by 3 so its annual
-    angle = info[0][0]
-    power = np.sum(info[1][0])/len(info[1][0])
-    speed = np.sum(info[1][2])/len(info[1][2])
-    velocity = np.sum(info[1][1])/len(info[1][1])
-    wind_angle = np.arctan2(u10, v10) + math.pi
-    angle_difference = abs(wind_angle - angle)
-    angle_difference_width = angle_difference-width/2
-    angle_difference = np.where(angle_difference>(2*math.pi-width/2), 1, 0)
-    angle_difference = np.where(angle_difference_width>0, 1, 0)
-    all_angle = np.sum(angle_difference)/315360
-    gen_list.append(gen)
-    energy_list.append(energy)
-    angle_list.append(angle)
-    all_angle_list.append(all_angle)
-    power_list.append(power)
-    speed_list.append(speed)
-    velocity_list.append(velocity)
-    y.append(coord[0]-25)
-    x.append(coord[1]+125)
-
+    # all_flywheel = all_flywheel_gen_energy(info[1][0],6,6)
+# #
+# #     # energy = info[0][1]/3 #divided by 3 so its annual
+# #     # angle = info[0][0]
+# #     # power = np.sum(info[1][0])/len(info[1][0])
+# #     # speed = np.sum(info[1][2])/len(info[1][2])
+# #     #
+# #     # velocity = np.sum(info[1][1])/len(info[1][1])
+# #     #
+# #     # gen = generator_classification(info[1][0],info[0][1],2,.8)[0]
+# #
+# #
+# #     # wind_angle = np.arctan2(u10, v10) + math.pi
+# #     # angle_difference = abs(wind_angle - angle)
+# #     # angle_difference_width = angle_difference-width/2
+# #     # angle_difference = np.where(angle_difference>(2*math.pi-width/2), 1, 0)
+# #     # angle_difference = np.where(angle_difference_width>0, 1, 0)
+# #     # all_angle = np.sum(angle_difference)/315360
+# #     # gen_list.append(gen)
+# #     # energy_list.append(energy)
+# #     # angle_list.append(angle)
+# #     # all_angle_list.append(all_angle)
+# #     # power_list.append(power)
+# #     # speed_list.append(speed)
+# #     # velocity_list.append(velocity)
+    # flywheel_list.append(flywheel)
+    # flywheel_gen_list.append(flywheel_gen)
+    # flywheel_gen_plain_list.append(flywheel_gen_plain)
+    # all_flywheel_list.append(all_flywheel)
+#     # y.append(coord[0]-25)
+#     # x.append(coord[1]+125)
+#
+# os.chdir('/media/sophie/3aad97f1-cb33-412d-b7f3-a82f0fc88a34')
+# pickle.dump(all_flywheel_list,open('all_flywheel_list6p.txt', 'wb'))
+# pickle.dump(flywheel_list,open('flywheel_list.txt', 'wb'))
+# pickle.dump(flywheel_gen_list,open('flywheel_gen_list.txt', 'wb'))
+# pickle.dump(flywheel_gen_plain_list,open('flywheel_gen_plain_list.txt', 'wb'))
+# pickle.dump(gen_list,open('gen_list180.txt', 'wb'))
+# pickle.dump(energy_list,open('energy_list180.txt', 'wb'))
+# pickle.dump(angle_list,open('angle_list180.txt', 'wb'))
+# pickle.dump(all_angle_list,open('all_angle_list180.txt', 'wb'))
+# pickle.dump(power_list,open('power_list180.txt', 'wb'))
+# pickle.dump(speed_list,open('speed_list180.txt', 'wb'))
+# pickle.dump(velocity_list,open('velocity_list180.txt', 'wb'))
+# pickle.dump(x,open('x.txt', 'wb'))
+# pickle.dump(y,open('y.txt', 'wb'))
+#
 os.chdir('/media/sophie/3aad97f1-cb33-412d-b7f3-a82f0fc88a34')
-pickle.dump(gen_list,open('gen_list.txt180', 'wb'))
-pickle.dump(energy_list,open('energy_list.txt180', 'wb'))
-pickle.dump(angle_list,open('angle_list.txt180', 'wb'))
-pickle.dump(all_angle_list,open('all_angle_list180.txt', 'wb'))
-pickle.dump(power_list,open('power_list.txt180', 'wb'))
-pickle.dump(speed_list,open('speed_list.txt180', 'wb'))
-pickle.dump(velocity_list,open('velocity_list180.txt', 'wb'))
-pickle.dump(x,open('x.txt', 'wb'))
-pickle.dump(y,open('y.txt', 'wb'))
-
-os.chdir('/media/sophie/3aad97f1-cb33-412d-b7f3-a82f0fc88a34')
+flywheel_list = pickle.load(open('flywheel_list.txt', 'rb'))
+flywheel_gen_list = pickle.load(open('flywheel_gen_list.txt', 'rb'))
+flywheel_gen_plain_list = pickle.load(open('flywheel_gen_plain_list.txt', 'rb'))
 gen_list = pickle.load(open('gen_list180.txt', 'rb'))
 energy_list = np.array(pickle.load(open('energy_list180.txt', 'rb')))
-angle_list = np.array(pickle.load(open('angle_list.txt180', 'rb')))
+angle_list = np.array(pickle.load(open('angle_list180.txt', 'rb')))
 all_angle_list = np.array(pickle.load(open('all_angle_list180.txt', 'rb')))
 power_list = np.array(pickle.load(open('power_list180.txt', 'rb')))
 speed_list = np.array(pickle.load(open('speed_list180.txt', 'rb')))
 velocity_list = np.array(pickle.load(open('velocity_list180.txt', 'rb')))
 x = pickle.load(open('x.txt', 'rb'))
 y = pickle.load(open('y.txt', 'rb'))
+
+
+
+##FLYWHEEL
+plt.figure(figsize = (5,20))
+plt.subplot(3,1,1)
+#creates scatterplot
+plt.scatter(x,y,c = np.log10(flywheel_list),label = 'Weather Site',cmap ='jet')
+#adds title
+plt.title('Annual Energy ', fontsize = 15)
+#adds axis
+plt.xticks(ticks = [0,10,20,30,40,50,60], labels = [-125,-115,-105,-95,-85,-75,-65], fontsize = 15)
+plt.yticks(ticks = [0,5,10,15,20,25], labels = [25,30,35,40,45,50], fontsize = 15)
+
+cbar = plt.colorbar(cmap = 'jet')
+
+cbar.ax.tick_params(labelsize=15)
+
+plt.clim(7.5, 9.5);
+
+plt.subplot(3,1,2)
+plt.scatter(x,y,c = np.log10(flywheel_gen_list),label = 'Weather Site',cmap ='jet')
+
+
+#adds axis
+plt.xticks(ticks = [0,10,20,30,40,50,60], labels = [-125,-115,-105,-95,-85,-75,-65], fontsize = 15)
+plt.yticks(ticks = [0,5,10,15,20,25], labels = [25,30,35,40,45,50], fontsize = 15)
+
+plt.ylabel('Latitude', fontsize = 20)
+
+cbar = plt.colorbar(cmap = 'jet')
+
+cbar.ax.tick_params(labelsize=15)
+
+cbar.set_label(label = 'Log10 of Average Annual Energy (J)', fontsize = 20)
+plt.clim(7.5, 9.5);
+
+plt.subplot(3,1,3)
+plt.scatter(x,y,c = np.log10(flywheel_gen_plain_list),label = 'Weather Site',cmap ='jet')
+
+#adds axis
+plt.xticks(ticks = [0,10,20,30,40,50,60], labels = [-125,-115,-105,-95,-85,-75,-65], fontsize = 15)
+plt.yticks(ticks = [0,5,10,15,20,25], labels = [25,30,35,40,45,50], fontsize = 15)
+plt.xlabel('Longitude', fontsize = 20)
+
+
+cbar = plt.colorbar(cmap = 'jet')
+
+cbar.ax.tick_params(labelsize=15)
+
+plt.clim(7.5, 9.5);
+plt.show()
+
+
+
 
 ####GENERATOR SIZE
 
@@ -348,7 +457,7 @@ plt.show()
 #creates scatterplot
 plt.scatter(x,y,c = np.log10(energy_list),label = 'Weather Site',cmap ='jet')
 #adds title
-plt.title('Energy Production in a Year', fontsize = 25)
+plt.title('Annual Energy', fontsize = 25)
 #adds axis
 plt.xticks(ticks = [0,10,20,30,40,50,60], labels = [-125,-115,-105,-95,-85,-75,-65], fontsize = 15)
 plt.yticks(ticks = [0,5,10,15,20,25], labels = [25,30,35,40,45,50], fontsize = 15)
@@ -361,7 +470,7 @@ cbar = plt.colorbar(cmap = 'jet')
 cbar.ax.tick_params(labelsize=15)
 
 cbar.set_label(label = 'Log10 of Average Annual Energy (J)', fontsize = 20)
-plt.clim(6.5, 7.75);
+plt.clim(8, 9.5);
 plt.show()
 
 
@@ -405,7 +514,7 @@ cbar = plt.colorbar(cmap = 'jet')
 cbar.ax.tick_params(labelsize=15)
 
 cbar.set_label(label = 'Speed (m/s)', fontsize = 20)
-plt.clim(2, 7);
+plt.clim(1, 7);
 plt.show()
 
 
@@ -427,7 +536,7 @@ cbar = plt.colorbar(cmap = 'jet')
 cbar.ax.tick_params(labelsize=15)
 
 cbar.set_label(label = 'Velocity (m/s)', fontsize = 20)
-plt.clim(1, 6);
+plt.clim(1, 7);
 plt.show()
 
 
@@ -447,4 +556,25 @@ plt.ylabel('Latitude', fontsize = 20)
 #adds legend
 #plt.legend(loc=3, fontsize = 15)
 
-#plt.show()
+plt.show()
+
+
+            ###VELOCITY/SPEED
+#creates scatterplot
+plt.scatter(x,y,c = velocity_list/speed_list,label = 'Weather Site',cmap ='jet')
+#adds title
+plt.title('Velocity/Speed', fontsize = 25)
+#adds axis
+plt.xticks(ticks = [0,10,20,30,40,50,60], labels = [-125,-115,-105,-95,-85,-75,-65], fontsize = 15)
+plt.yticks(ticks = [0,5,10,15,20,25], labels = [25,30,35,40,45,50], fontsize = 15)
+plt.xlabel('Longitude', fontsize = 20)
+plt.ylabel('Latitude', fontsize = 20)
+#adds legend
+plt.legend(loc=3, fontsize = 15)
+cbar = plt.colorbar(cmap = 'jet')
+
+cbar.ax.tick_params(labelsize=15)
+
+#cbar.set_label( fontsize = 20)
+plt.clim(.5,1);
+plt.show()
